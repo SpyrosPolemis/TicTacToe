3bf9ee1f7ba0eb2c38d255a24a5fc7b10f4be814
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2025-09-17 17:06:48 +1000

    Progress on createSquare function

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Howdy partner")
+let gameboard = [null * 9]
+
+function createSquare() {
+    let status = null;
+    markX = function() {
+        status = "X";
+    }
+    markY = function() {
+        status = "Y";
+    }
+    getStatus = function() {
+        return status;
+    }
+
+    return {markX, markY, getStatus}
+}
