a3d5de2ca7fa04aa7994c7faf4c2f4a8a3bf1c03
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2025-09-22 22:43:27 +1000

    Start checkForWinner function

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
-let gameboard = [null * 9]
+let gameboard = [createSquare(), createSquare(), createSquare(), createSquare(), createSquare(), createSquare(), createSquare(), createSquare(), createSquare()]
 
 function createSquare() {
     let status = null;
     markX = function() {
         status = "X";
+        checkForWinner()
     }
     markY = function() {
         status = "Y";
+        checkForWinner()
     }
     getStatus = function() {
         return status;
     }
 
     return {markX, markY, getStatus}
+}
+
+function checkForWinner() {
+    if (gameboard[0].getStatus() === gameboard[1].getStatus() && gameboard[0].getStatus() === gameboard[2].getStatus()) {
+        console.log("Winner is X")
+    } else {
+        console.log("No winner yet")
+    }
+}
+
+// Testing
+
+function populateBoard() {
+    for (let i  = 0; i < 9; i++) {
+        gameboard[i] = createSquare()
+    }
+    for (let i  = 0; i < 9; i++) {
+        gameboard[i].markX()
+    }
 }
